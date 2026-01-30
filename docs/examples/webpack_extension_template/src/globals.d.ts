1aebe95cf4417d95559e5cbdf75e9a5f912f6f5b
Prolix <me@prolix.live>
2026-01-30 16:05:34 -0500

    Build with new webpack, remove old docs

--- docs/examples/webpack_extension_template/src/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// global.d.ts to handle SillyTavern types
-declare global {
-    var SillyTavern: any;
-    var $: any;
-    var jQuery: any;
-}
-
-export {};
--- src/lib/version.js
+++ src/lib/version.js
@@ -10,7 +10,7 @@
  * Updated automatically during deployment
  * @type {string}
  */
-export const EXTENSION_VERSION = "4.0.17";
+export const EXTENSION_VERSION = "4.0.18";
 
 /**
  * Export as default for convenience
